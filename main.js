6f01ee7eb24b70c82569f6ece80e9db90b8ab724
Unikamo <foxtrot.4874@gmail.com>
2024-12-15 22:09:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,21 +3,21 @@ const textElement = document.getElementById("text1");
 let index = 0;
 
 function typeText() {
-    if (index < text.length) {
-        textElement.innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeText, 125);
-    } else {
-        addCursor();
-    }
+    	if (index < text.length) {
+        	textElement.innerHTML += text.charAt(index);
+        	index++;
+        	setTimeout(typeText, 125);
+    	} else {
+        	addCursor();
+    	}
 }
 
 function addCursor() {
-    const cursor = document.createElement('span');
-    cursor.className = 'cursor';
-    cursor.innerHTML = "_";
-    textElement.appendChild(cursor);
-    cursor.style.left = `${textElement.offsetWidth}px`;
+	const cursor = document.createElement('span');
+	cursor.className = 'cursor';
+	cursor.innerHTML = "_";
+    	textElement.appendChild(cursor);
+    	cursor.style.left = `${textElement.offsetWidth}px`;
 }
 
 function math() {
